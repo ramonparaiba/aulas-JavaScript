cccb6e158ce0e5de2ab9c8ec6819632bca584f42
pablo <ramonparaiba@gmail.com>
2023-05-05 11:23:47 -0300

    class: function

---
+++ 01-basic-sintaxe/002-funcoes.js
@@ -0,0 +1,21 @@
+//Estrutura básica 
+
+function NomeDaFuncao(){
+    console.log('Uma mensagem');
+}
+NomeDaFuncao()
+//Passagem de parâmetro
+
+function square(num){
+    console.log(num);
+}
+
+square(6)
+
+//Retornando valores
+
+function half(num){
+    return num/2
+}
+
+console.log(half(8));
